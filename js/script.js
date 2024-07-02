23a575e72221c7b5e6abbc6116bc65bd10368e83
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-01 20:38:32 -0400

    Add displaying bars

--- js/script.js
+++ js/script.js
@@ -11,13 +11,14 @@ const svg = d3.select('.chart')
 
 d3.json(gdpDataUrl).then((jsonResponse) => {
   const dataset = jsonResponse.data;
-  const barWidth = width / dataset.width;
+  const barWidth = width / dataset.length;
+  console.log(barWidth)
 
   // Setting up xScale
   const years = dataset.map(([year]) => year.slice(0, 4));
-  const yearsToDate = years.map((year) => new Date(year));
+  const yearsToDate = dataset.map(([year]) => new Date(year));
   const maxYear = d3.max(yearsToDate);
-  maxYear.setMonth(maxYear.getMonth() + 6);
+  maxYear.setMonth(maxYear.getMonth());
 
   const xScale = d3
     .scaleTime()
@@ -37,19 +38,41 @@ d3.json(gdpDataUrl).then((jsonResponse) => {
   const maxGdp = d3.max(gdp);
   const yScale = d3
     .scaleLinear()
-    .domain([d3.min(gdp), maxGdp])
+    .domain([0, maxGdp])
     .range([height - padding, padding]);
 
+  const yAxisScale = d3
+    .scaleLinear()
+    .domain([0, maxGdp])
+    .range([height - padding, 0]);
+
   // Adding yAxis
-  const yAxis = d3.axisLeft().scale(yScale);
+  const yAxis = d3.axisLeft().scale(yAxisScale);
 
   svg
     .append('g')
     .call(yAxis)
     .attr('transform', `translate(${padding}, 0)`);
 
+  const linearScale = d3.scaleLinear().domain([0, maxGdp]).range([0, height - padding]);
+  const scaledGDP = gdp.map(function (item) {
+    return linearScale(item);
+  });
 
-
+  svg
+    .selectAll('rect')
+    .data(scaledGDP)
+    .enter()
+    .append('rect')
+    .attr('x', (_d, i) => {
+      console.log(xScale(yearsToDate[i]));
+      return xScale(yearsToDate[i]);
+    })
+    .attr('y', (d) => height - d)
+    .attr('width', barWidth)
+    .attr('height', (d) => d)
+    .style('fill', '#3182bd')
+    .attr('transform', `translate(0, -${padding})`);
 
 }).catch((e) => {
   console.log(e);
